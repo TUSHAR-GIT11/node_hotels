1c500993b4c8bc527226e97ad92249ac77596cfd
TUSHAR-GIT11 <tushargupta10th@gmail.com>
2025-04-06 22:18:12 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 
 const db = require('./db')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+const PORT = process.env.PORT || 3000
+
 
 
 const personRoutes = require('./routes/personRoutes')
 app.use('/person',personRoutes)  
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
   console.log("app running on port 3000")
 })
 
